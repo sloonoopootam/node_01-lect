06d34a8f8d6bd7052b92f12789f275bc46e6c589
sloonoopootam <saykoea@gmail.com>
2019-12-02 15:39:41 +0200

    Working with stream

--- stream.js
+++ stream.js
@@ -9,7 +9,7 @@
 **      in the stream and additionally new data: the date of entry, name, surname and rank.
  */
 
-// (01)
+// ( 01 )
 // const fs = require('fs');
 // const file = fs.createWriteStream('./almost-big.file');
 //
@@ -20,7 +20,7 @@
 //
 // file.end();
 //
-// (02)
+// ( 02 )
 // const fs = require('fs');
 // const server = require('http').createServer();
 //
@@ -31,7 +31,7 @@
 //
 // server.listen(8000);
 //
-// (03)
+// ( 03 )
 // const {Duplex} = require('stream');
 // const fs = require('fs');
 //
@@ -45,8 +45,11 @@
 //
 // process.stdin.pipe(inoutStream);
 //
-// (04 - 1)
+// ( 04 )
 // const {Transform} = require('stream');
+// const fs = require('fs');
+// const file1 = fs.createWriteStream('./main.file');
+// const file2 = fs.createWriteStream('./minor.file');
 //
 // const upperCaseTr = new Transform({
 //     transform(chunk, encoding, callback) {
@@ -55,21 +58,19 @@
 //     }
 // });
 //
-// process.stdin
-//     .pipe(upperCaseTr)
-//     .pipe(process.stdout);
-//
-// (04 - 2)
-// const {Transform} = require('stream');
-//
 // const numberFreeTr = new Transform({
 //     transform(chunk, encoding, callback) {
 //         this.push(chunk.toString().replace(/[0-9]/g, ''));
+//         if (chunk.toString().replace(/[^0-9]/g, '').length > 0) {
+//             file2.write(`${chunk.toString().replace(/[^0-9]/g, '')} : ${new Date}, ` +
+//              `${this.firstname} ${this.lastname}, ${this.rank}`; // ( 05 )
+//         }
 //         callback();
 //     }
 // });
 //
 // process.stdin
+//     .pipe(upperCaseTr)
 //     .pipe(numberFreeTr)
 //     .pipe(process.stdout);
 //
@@ -87,4 +88,3 @@
 // process.stdin
 //     .pipe(numberFirstUpTr)
 //     .pipe(process.stdout);
-
